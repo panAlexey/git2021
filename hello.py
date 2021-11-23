ead7400ac16af6ef034dedeff13b399b8122ff7b
pan Alexey <mr.alex.pan@list.ru>
2021-11-23 21:05:03 +0300

    add print

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ def main():
     print('My first git program')
     print('exe')
     print('hi people2')
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
